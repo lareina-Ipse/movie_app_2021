ea06e2895126ccdf6d8e28d06a75634b8f7ff675
lareina_Ipse <lareina.ipse@gmail.com>
2021-01-14 11:47:52 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 
 import React from "react";
-
-function Food({name, picture}) {
-
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
-    image: "http://aeriskitchen.com/wp-content/uploads/2018/09/moms_best_kimchi_01-.jpg"
+    image: "http://aeriskitchen.com/wp-content/uploads/2018/09/moms_best_kimchi_01-.jpg",
+    rating: 5
   },
   {
+    id: 2,
     name: "Bibimbap",
-    image: "http://aeriskitchen.com/wp-content/uploads/2019/02/Stone_Bowl_Bibimbap_00-1-1.jpg"
+    image: "http://aeriskitchen.com/wp-content/uploads/2019/02/Stone_Bowl_Bibimbap_00-1-1.jpg",
+    rating: 4.9
   }
 ]
+
+function Food({name, picture, rating}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+}
+
+Food.PropTypes = {
+
+}
 
 function App() {
   return (
     <div className="App">
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
+      {foodILike.map(dish => (
+      <Food 
+        key={dish.id} 
+        name={dish.name} 
+        picture={dish.image}
+        rating={dish.rating}  
+      />
+      ))}
     </div>
   );
 }
